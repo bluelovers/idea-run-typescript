d714f765d3f78da1f59781264a88c573f71f8865
bluelovers <codelovers@users.sourceforge.net>
2018-08-08 13:53:17 +0800

    :bug: fix cannot be cast to org.jetbrains.debugger.DebuggableRunConfiguration

--- src/io/plugin/tsnode/execution/TsProgramRunner.kt
+++ src/io/plugin/tsnode/execution/TsProgramRunner.kt
@@ -1,21 +1,33 @@
 package io.plugin.tsnode.execution
 
 //import com.intellij.execution.runners.DefaultProgramRunner
+//import com.intellij.javascript.debugger.execution.DebuggableProgramRunner
 import com.intellij.execution.configurations.RunProfile
 import com.intellij.execution.executors.DefaultRunExecutor
-import com.intellij.javascript.debugger.execution.DebuggableProgramRunner
+import com.intellij.execution.runners.DefaultProgramRunner
 import io.plugin.tsnode.lib.TsLog
 
-class TsProgramRunner : DebuggableProgramRunner()
+class TsProgramRunner : DefaultProgramRunner()
 {
 	override fun getRunnerId() = javaClass.simpleName
+
+	val LOG = TsLog(javaClass)
 
 	override fun canRun(executorId: String, profile: RunProfile): Boolean
 	{
 		val bool = DefaultRunExecutor.EXECUTOR_ID == executorId && profile is TsRunConfiguration
 
-		TsLog(javaClass).info("[canRun] $bool $executorId $profile")
+		//LOG.info("[canRun] $bool $executorId $profile")
 
 		return bool
 	}
+
+	/*
+	override fun execute(environment: ExecutionEnvironment, state: RunProfileState): Promise<RunContentDescriptor?>
+	{
+		LOG.info("[execute] $environment $state")
+
+		return super.execute(environment, state)
+	}
+	*/
 }
