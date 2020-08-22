52a0503aa20250ed9e8d633f2094df3f7a18db1d
bluelovers <codelovers@users.sourceforge.net>
2020-08-23 06:18:57 +0800

    build: add copy plugin.zip from build/distributions to releases

--- scripts/copy-build-plugin.ts
+++ scripts/copy-build-plugin.ts
@@ -6,7 +6,11 @@ import { join, relative } from "path";
 let file = join(__root, 'build', 'libs', `idea-run-typescript-${pkg.version}.jar`);
 let target = join(__root, 'releases', 'idea-run-typescript.jar');
 
+let file2 = join(__root, 'build', 'distributions', `idea-run-typescript-${pkg.version}.zip`);
+let target2 = join(__root, 'releases', 'idea-run-typescript.zip');
+
 let label = [`copy`, relative(__root, file), `=>`, relative(__root, target)].join(' ');
+let label2 = [`copy`, relative(__root, file2), `=>`, relative(__root, target2)].join(' ');
 
 console.time(label);
 
@@ -17,3 +21,13 @@ copySync(file, target, {
 });
 
 console.timeEnd(label);
+
+console.time(label2);
+
+copySync(file2, target2, {
+	preserveTimestamps: true,
+	overwrite: true,
+	dereference: true,
+});
+
+console.timeEnd(label2);
