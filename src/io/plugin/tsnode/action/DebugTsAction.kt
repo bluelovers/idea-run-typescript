51cbdd1dc7b2f66b929f61e458e04f24a35511fc
bluelovers <codelovers@users.sourceforge.net>
2020-08-23 03:50:51 +0800

    todo: make `TsDebugProgramRunner` can be fully replace `DebugTsAction`

--- src/io/plugin/tsnode/action/DebugTsAction.kt
+++ src/io/plugin/tsnode/action/DebugTsAction.kt
@@ -2,7 +2,9 @@ package io.plugin.tsnode.action
 
 import icons.TsIcons
 
-@Deprecated("do not use this")
+/**
+ * @todo make `TsDebugProgramRunner` can be fully replace `DebugTsAction`
+ */
 class DebugTsAction : TsAction(TsIcons.TypeScriptDebug)
 {
 	override val _debug = true
