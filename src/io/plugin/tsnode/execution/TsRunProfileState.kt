77db096f13c4b0b19fc5f2fbff1d8e929a8441f1
bluelovers <codelovers@users.sourceforge.net>
2020-08-24 05:01:37 +0800

    fix: https://github.com/bluelovers/idea-run-typescript/issues/15
    TODO: support `ctrl+c`

--- src/io/plugin/tsnode/execution/TsRunProfileState.kt
+++ src/io/plugin/tsnode/execution/TsRunProfileState.kt
@@ -1,37 +1,21 @@
 package io.plugin.tsnode.execution
 
-import com.intellij.execution.DefaultExecutionResult
 import com.intellij.execution.ExecutionResult
 import com.intellij.execution.Executor
 import com.intellij.execution.configurations.CommandLineState
 import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.execution.configurations.RunProfileState
 import com.intellij.execution.process.KillableColoredProcessHandler
+import com.intellij.execution.process.OSProcessHandler
 import com.intellij.execution.process.ProcessHandler
-import com.intellij.execution.process.ProcessTerminatedListener
-import com.intellij.execution.runners.DebuggableRunProfileState
 import com.intellij.execution.runners.ExecutionEnvironment
-import com.intellij.execution.testframework.TestConsoleProperties
-import com.intellij.execution.testframework.autotest.ToggleAutoTestAction
-import com.intellij.execution.testframework.sm.SMTestRunnerConnectionUtil
-import com.intellij.execution.testframework.sm.runner.ui.SMTRunnerConsoleView
-import com.intellij.execution.ui.ConsoleView
 import com.intellij.javascript.debugger.CommandLineDebugConfigurator
-import com.intellij.javascript.nodejs.NodeCommandLineUtil
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.util.execution.ParametersListUtil
 import io.plugin.tsnode.execution.TsUtil.tsnodePath
 import io.plugin.tsnode.lib.TsLog
-import com.intellij.javascript.nodejs.debug.createDebugPortString
-import com.intellij.javascript.nodejs.debug.NodeLocalDebugRunProfileState
 import com.intellij.javascript.nodejs.debug.NodeLocalDebuggableRunProfileState
-import com.intellij.javascript.nodejs.interpreter.NodeJsInterpreter
-import com.intellij.javascript.nodejs.interpreter.NodeJsInterpreterRef
-import com.intellij.javascript.nodejs.interpreter.local.NodeJsLocalInterpreter
-import com.intellij.javascript.protractor.ProtractorConsoleFilter
-import com.intellij.javascript.protractor.ProtractorConsoleProperties
-import com.intellij.javascript.protractor.ProtractorRunState.FRAMEWORK_NAME
 import java.nio.charset.Charset
 import java.io.File
 import com.intellij.javascript.testFramework.navigation.JSTestLocationProvider
@@ -39,6 +23,8 @@ import com.intellij.lang.javascript.buildTools.base.JsbtUtil.foldCommandLine
 import com.intellij.openapi.util.Disposer
 import com.intellij.util.ThrowableConsumer
 import io.plugin.tsnode.execution.TsUtil.isEmptyOrSpacesOrNull
+import io.plugin.tsnode.lib.cmd.MyNodeCommandLineUtil
+import org.jetbrains.annotations.NotNull
 import org.jetbrains.concurrency.Promise
 import org.jetbrains.concurrency.resolvedPromise
 
@@ -53,27 +39,13 @@ class TsRunProfileState(protected var project: Project,
 	fun createCommandLine(): GeneralCommandLine
 	{
 		val runSettings = runConfig.runSettings
-		val commandLine = NodeCommandLineUtil.createCommandLine()
+		val commandLine = MyNodeCommandLineUtil.createCommandLine(runConfig, project)
 
-		if (this.debugConfigurator !== null) {
-			NodeCommandLineUtil.configureCommandLine(commandLine, debugConfigurator, ThrowableConsumer {  })
-		}
+		MyNodeCommandLineUtil.configureDebugConfigurator(commandLine, this.debugConfigurator)
 
-		if (StringUtil.isEmptyOrSpaces(runSettings.workingDirectory))
-		{
-			commandLine.withWorkDirectory(project.basePath)
-		}
-		else
-		{
-			commandLine.withWorkDirectory(runSettings.workingDirectory)
-		}
+		MyNodeCommandLineUtil.configureWorkDirectory(commandLine, runSettings, project)
 
-		commandLine.withEnvironment(this.runConfig.envs)
-
-		/**
-		 * same as node.js run add FORCE_COLOR=true
-		 */
-		commandLine.environment.putIfAbsent("FORCE_COLOR", "3")
+		MyNodeCommandLineUtil.configureEnvironment(commandLine, this.runConfig.envs)
 
 		commandLine.exePath = runConfig.getInterpreterSystemDependentPath()
 
@@ -121,14 +93,11 @@ class TsRunProfileState(protected var project: Project,
 		return commandLine
 	}
 
-	override fun startProcess(): ProcessHandler
+	override fun startProcess(): OSProcessHandler
 	{
 		val commandLine = createCommandLine()
 
-		if (commandLine.charset.toString() == "x-windows-950" || commandLine.charset.toString() == "x-windows-936")
-		{
-			commandLine.charset = Charset.forName("UTF-8")
-		}
+		MyNodeCommandLineUtil.configureCharset(commandLine)
 
 //		val processHandler = KillableColoredProcessHandler(commandLine)
 //		/**
@@ -137,11 +106,7 @@ class TsRunProfileState(protected var project: Project,
 //		 */
 //		processHandler.setShouldKillProcessSoftlyWithWinP(true)
 
-		val processHandler = NodeCommandLineUtil.createProcessHandler(commandLine, true)
-
-		ProcessTerminatedListener.attach(processHandler)
-
-		return processHandler
+		return MyNodeCommandLineUtil.createProcessHandler(commandLine, project, this.debugConfigurator)
 	}
 
 	override fun execute(configurator: CommandLineDebugConfigurator?): Promise<ExecutionResult> {
---
+++ src/io/plugin/tsnode/lib/cmd/NodeJSCommandLine.kt
@@ -0,0 +1,169 @@
+package io.plugin.tsnode.lib.cmd
+
+import com.intellij.execution.configurations.GeneralCommandLine
+import com.intellij.execution.configurations.PtyCommandLine
+import com.intellij.execution.process.OSProcessHandler
+import com.intellij.execution.process.ProcessTerminatedListener
+import com.intellij.javascript.debugger.CommandLineDebugConfigurator
+import com.intellij.javascript.nodejs.NodeCommandLineUtil
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.registry.Registry
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.util.ThrowableConsumer
+import io.plugin.tsnode.execution.TsRunConfiguration
+import io.plugin.tsnode.execution.TsRunSettings
+import java.nio.charset.Charset
+
+object MyNodeCommandLineUtil
+{
+
+	fun createCommandLine(runConfig: TsRunConfiguration?, project: Project?): GeneralCommandLine
+	{
+		var usePtyWithTerminalConsole: Boolean? = null
+
+		if (runConfig != null)
+		{
+			/**
+			 * allow set env var nodejs.console.use.terminal=true/false for enable or disable
+			 */
+			val v = runConfig.runSettings.envData.envs.get("nodejs.console.use.terminal")
+
+			if (!v.isNullOrEmpty() && !v.isBlank())
+			{
+				usePtyWithTerminalConsole = v.toBoolean()
+			}
+		}
+
+		if (usePtyWithTerminalConsole == null)
+		{
+			/**
+			 * use idea Registry nodejs.console.use.terminal
+			 */
+			usePtyWithTerminalConsole = Registry.`is`("nodejs.console.use.terminal")
+		}
+
+		val commandLine = NodeCommandLineUtil.createCommandLine(usePtyWithTerminalConsole)
+
+		if (NodeCommandLineUtil.isTerminalCommandLine(commandLine))
+		{
+			// do something
+			(commandLine as PtyCommandLine)
+		}
+		else
+		{
+			// do something
+		}
+
+		return commandLine
+	}
+
+	fun createCommandLine(): GeneralCommandLine
+	{
+		return createCommandLine(null, null)
+	}
+
+	fun createCommandLine(runConfig: TsRunConfiguration?): GeneralCommandLine
+	{
+		return createCommandLine(runConfig, null)
+	}
+
+	fun createCommandLine(project: Project?): GeneralCommandLine
+	{
+		return createCommandLine(null, project)
+	}
+
+	fun configureDebugConfigurator(commandLine: GeneralCommandLine, debugConfigurator: CommandLineDebugConfigurator?)
+	{
+		if (debugConfigurator !== null) {
+			NodeCommandLineUtil.configureCommandLine(commandLine, debugConfigurator, ThrowableConsumer { })
+		}
+	}
+
+	fun configureWorkDirectory(commandLine: GeneralCommandLine, runSettings: TsRunSettings, project: Project)
+	{
+		if (StringUtil.isEmptyOrSpaces(runSettings.workingDirectory))
+		{
+			commandLine.withWorkDirectory(project.basePath)
+		}
+		else
+		{
+			commandLine.withWorkDirectory(runSettings.workingDirectory)
+		}
+	}
+
+	fun configureEnvironment(commandLine: GeneralCommandLine, envs: Map<String, String>?)
+	{
+		if (envs !== null) {
+			commandLine.withEnvironment(envs)
+		}
+
+		val env = commandLine.environment;
+
+		/**
+		 * same as node.js run add FORCE_COLOR=true
+		 */
+		env.putIfAbsent("FORCE_COLOR", "3")
+
+		NodeCommandLineUtil.configureUsefulEnvironment(commandLine)
+
+		/*
+		env.putIfAbsent("COLORTERM", "true")
+		env.putIfAbsent("FORCE_COLOR", "true")
+		env.putIfAbsent("npm_config_color", "always")
+		env.putIfAbsent("MOCHA_COLORS", "1")
+		 */
+	}
+
+	fun configureCharset(commandLine: GeneralCommandLine)
+	{
+		if (commandLine.charset.toString() == "x-windows-950" || commandLine.charset.toString() == "x-windows-936")
+		{
+			commandLine.withCharset(Charset.forName("UTF-8"))
+		}
+	}
+
+	fun createProcessHandler(commandLine: GeneralCommandLine, project: Project?, debugConfigurator: CommandLineDebugConfigurator?): OSProcessHandler
+	{
+		val processHandler = if (NodeCommandLineUtil.isTerminalCommandLine(commandLine))
+		{
+			val p = NodeCommandLineUtil.createKillableColoredProcessHandler(commandLine, true)
+
+			p.setShouldKillProcessSoftlyWithWinP(true)
+
+			p
+		}
+		else
+		{
+			NodeCommandLineUtil.createProcessHandler(commandLine, true, debugConfigurator)
+		}
+
+		configureProcess(processHandler, project)
+
+		return processHandler
+	}
+
+	fun createProcessHandler(commandLine: GeneralCommandLine): OSProcessHandler
+	{
+		return createProcessHandler(commandLine, null, null)
+	}
+
+	fun configureProcess(processHandler: OSProcessHandler, project: Project?)
+	{
+		processHandler.setShouldDestroyProcessRecursively(true);
+
+		if (project != null)
+		{
+			ProcessTerminatedListener.attach(processHandler, project)
+		}
+		else
+		{
+			ProcessTerminatedListener.attach(processHandler)
+		}
+	}
+
+	fun configureProcess(processHandler: OSProcessHandler)
+	{
+		return configureProcess(processHandler, null)
+	}
+
+}
